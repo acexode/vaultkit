f20a0118d5654a6e81521501dac9ee808f1cfade
Sir Abubakar <abudawud92@gmail.com>
2024-06-23 17:26:39 +0100

    ui changes

---
+++ src/utils/common-utils.js
@@ -0,0 +1,4 @@
+export const convertToSentenceCase =(str)=> {
+    str = str.replace(/_/g, ' ');
+    return str.toLowerCase().replace(/(^|[.!?])(\w)/g, (match, p1, p2) => p1 + p2.toUpperCase());
+}
